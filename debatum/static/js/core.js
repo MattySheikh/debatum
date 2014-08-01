36e0ce5675d39166893e9c80d7d449d7bc23228e
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-01 12:23:04 -0700

    Better login flow

--- debatum/static/js/core.js
+++ debatum/static/js/core.js
@@ -1,3 +1,17 @@
 (function(document) {
 	var $d = $(document);
+	var toggleLogin = function() {
+		var $this = $(this);
+		$this.hide();
+		$this.siblings('.login-header').fadeIn(200);
+		$this.parent().siblings('.header-inline').hide();
+	};
+
+	var closeLogin = function() {
+		$('.login-header').hide();
+		$('.header-inline, .header-action').fadeIn(200);
+	}
+	$d
+	.on('click', '.header-action', toggleLogin)
+	.on('click', '.close-login', closeLogin)
 })(document);
