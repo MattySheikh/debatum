28621cb2e8f0694e43c31f231f40910fcffdf601
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-01 11:35:25 -0700

    Fuck Box

--- debatum/static/js/core.js
+++ debatum/static/js/core.js
@@ -1,26 +1,3 @@
 (function(document) {
 	var $d = $(document);
-
-	var wrapBox = function(el) {
-		var $wrap = '',
-			$this = $(el),
-			$old = $this.html();
-		$wrap += ''
-			+ '<div class="box-container">'
-				+ '<div class="box-blur"></div>'
-				+ '<div class="box-inner">'
-					+ $old
-				+ '</div>'
-			+ '</div>';
-		$this.html($wrap);
-		$('.box-container, .box-wrap').show();
-	}
-	$d.on('ready', function() {
-		$('.header-action').on('click', function() {
-			var $this = $(this),
-				active = $this.hasClass('header-log-in') ? '#login-box' : '#signup-box';
-			wrapBox($d.find('.box-wrap'));
-			$(active).show();
-		});
-	});
 })(document);
