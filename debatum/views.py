8cf5fc66f66ab26deb0d3c58a5ddbb9b4f3b766f
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-07-31 19:54:16 -0700

    Figured out contexts for CSS

--- debatum/views.py
+++ debatum/views.py
@@ -1,5 +1,9 @@
-from django.views import generic
+from django.views.generic.base import TemplateView
 from django.template import RequestContext
 
-class Index(generic.TemplateView):
+class Index(TemplateView):
 	template_name = 'index.html'
+	def get_context_data(self, **kwargs):
+		context = super(Index, self).get_context_data(**kwargs)
+		context['css'] = ['homepage']
+		return context
