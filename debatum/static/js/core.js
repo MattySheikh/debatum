bf64f8af7589699a6ac94f5f7ac231ee236f6a7c
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-01 09:55:13 -0700

    Adds and styles login info

---
+++ debatum/static/js/core.js
@@ -0,0 +1,11 @@
+(function(window, document) {
+	document.addEventListener("DOMContentLoaded", function() {
+		document.getElementById('header-log-in').onclick = function() {
+			
+		}
+
+		document.getElementById('header-sign-up').onclick = function() {
+			
+		}
+	})
+})(window, document);
