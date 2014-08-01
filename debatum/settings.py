46d14ba17222651da7291d4246def12e0bf69eed
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-07-31 17:49:02 -0700

    Install south and change postgres settings

--- debatum/settings.py
+++ debatum/settings.py
@@ -57,10 +57,15 @@ WSGI_APPLICATION = 'debatum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'debatum',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
