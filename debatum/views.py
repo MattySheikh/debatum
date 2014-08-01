e238ef3b6f2aa5798ac7bd984e6188df00b86c10
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-01 01:48:48 -0700

    Styles styles styles~!~!

--- debatum/views.py
+++ debatum/views.py
@@ -6,4 +6,5 @@ class Index(TemplateView):
 	def get_context_data(self, **kwargs):
 		context = super(Index, self).get_context_data(**kwargs)
 		context['css'] = ['homepage']
+		context['js'] = ['homepage']
 		return context
