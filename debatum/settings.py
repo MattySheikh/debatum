6052c299853e72b2d5add078e9b9efe2ab7d9f68
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-05 00:02:06 -0700

    Settings

--- debatum/settings.py
+++ debatum/settings.py
@@ -91,7 +91,10 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'debatum/templates')]
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2'
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
