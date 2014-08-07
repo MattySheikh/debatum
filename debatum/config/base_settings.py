e9fc4d1b707c26c77b25248a584ca50147bbc5c2
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-06 21:23:57 -0700

    Fixing static files

--- debatum/config/base_settings.py
+++ debatum/config/base_settings.py
@@ -64,7 +64,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'debatum/templates')]
 
@@ -84,7 +84,6 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
