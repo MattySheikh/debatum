0ffb727180837c1ced15fa05ada97b54dd4fd823
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-11 13:48:46 -0700

    Implicitly set DJANGO_SETTINGS_MODULE in wsgi

--- debatum/wsgi.py
+++ debatum/wsgi.py
@@ -10,4 +10,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "debatum.config.production_settings")
 application = Cling(get_wsgi_application())
