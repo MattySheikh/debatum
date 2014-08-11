25298f848561ffcd1100cd089bd5e86b92420ebd
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-08-11 14:25:07 -0700

    Fixing production settings database

--- debatum/config/production_settings.py
+++ debatum/config/production_settings.py
@@ -4,7 +4,6 @@ TEMPLATE_DEBUG = True
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES = {
-	'default': dj_database_url.config(),
-	'ENGINE': 'django.db.backends.postgresql_psycopg2'
+	'default': dj_database_url.config()
 }
 WSGI_APPLICATION = 'debatum.wsgi.application'
